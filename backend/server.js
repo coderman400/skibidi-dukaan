08897b69ea185550d44705355c32a0c681d0aa03
bala <krbala1511@gmail.com>
2025-04-24 23:31:26 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://skibidi-dukaan-chi.vercel.app'
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
